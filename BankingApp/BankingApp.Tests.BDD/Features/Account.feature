be58232b4af04377fceabd94bc7d5ba66e1d5f60
yann.courtel <yann.courtel@ext.cdiscount.com>
2021-04-28 17:00:14 +0200

    add BDd tests with specflow

---
+++ BankingApp/BankingApp.Tests.BDD/Features/Account.feature
@@ -0,0 +1,12 @@
+﻿Feature: An account
+
+Scenario: Can print a statement after with a deposit transaction
+	Given a client makes a deposit 500 euros on the 2021/04/27
+	When she prints her bank statement
+	Then she should see a statement with the following transactions:
+	"""
+	| Date       | Amount | RunningBalance |
+	| 2021/04/27 | +500   | 500            |
+	"""
+
+ 
---
+++ BankingApp/BankingApp.Tests.BDD/Steps/AccountStepDefinitions.cs
@@ -0,0 +1,34 @@
+﻿using TechTalk.SpecFlow;
+
+namespace BankingApp.Tests.BDD.Steps
+{
+    [Binding]
+    public sealed class AccountStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        public AccountStepDefinitions()
+        {
+
+        }
+
+        [Given(@"a client makes a deposit (.*) euros on the (.*)/(.*)")]
+        public void GivenAClientMakesADepositEurosOnThe(int p0, string p1, int p2)
+        {
+            
+        }
+        
+        [When(@"she prints her bank statement")]
+        public void WhenShePrintsHerBankStatement()
+        {
+            
+        }
+        
+        [Then(@"she should see a statement with the following transactions:")]
+        public void ThenSheShouldSeeAStatementWithTheFollowingTransactions(string multilineText)
+        {
+            
+        }
+
+    }
+}
